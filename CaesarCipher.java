00b7f615cff341cc9ccbb1e705fa833bc5c89d23
Mohammad Shariq Pir <pirshariq419@gmail.com>
2025-11-14 23:18:34 +0530

    Refactor CaesarCipher for inline encryption and decryption

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,36 +1,37 @@
 import java.util.*;
 
 public class CaesarCipher {
-    public static String encrypt(String text, int shift) {
-        StringBuilder result = new StringBuilder();
-        shift = shift % 26 + 26;
-        for (char i : text.toCharArray()) {
-            if (Character.isLetter(i)) {
-                if (Character.isUpperCase(i)) {
-                    result.append((char) ('A' + (i - 'A' + shift) % 26));
-                } else {
-                    result.append((char) ('a' + (i - 'a' + shift) % 26));
-                }
-            } else {
-                result.append(i);
-            }
-        }
-        return result.toString();
-    }
-
-    public static String decrypt(String text, int shift) {
-        return encrypt(text, 26 - shift);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter text: ");
         String text = sc.nextLine();
+
         System.out.print("Enter shift key: ");
         int key = sc.nextInt();
-        String encrypted = encrypt(text, key);
+
+        StringBuilder encrypted = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char base = Character.isUpperCase(c) ? 'A' : 'a';
+                encrypted.append((char) (base + (c - base + key) % 26));
+            } else {
+                encrypted.append(c);
+            }
+        }
         System.out.println("Encrypted: " + encrypted);
-        System.out.println("Decrypted: " + decrypt(encrypted, key));
+
+        StringBuilder decrypted = new StringBuilder();
+        for (char c : encrypted.toString().toCharArray()) {
+            if (Character.isLetter(c)) {
+                char base = Character.isUpperCase(c) ? 'A' : 'a';
+                decrypted.append((char) (base + (c - base - key + 26) % 26));
+            } else {
+                decrypted.append(c);
+            }
+        }
+        System.out.println("Decrypted: " + decrypted);
+
         sc.close();
     }
 }
