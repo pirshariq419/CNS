c58c4c741ae9d019e79c133ac9d6da278981a895
Mohammad Shariq Pir <pirshariq419@gmail.com>
2025-11-14 23:15:03 +0530

    Update SubstitutionCipher.java

--- SubstitutionCipher.java
+++ SubstitutionCipher.java
@@ -4,15 +4,25 @@ public class SubstitutionCipher {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String key = "QWERTYUIOPASDFGHJKLZXCVBNM"; // example key
+        String key = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
         System.out.print("Enter plaintext: ");
         String text = sc.nextLine().toUpperCase();
+
         StringBuilder cipher = new StringBuilder();
         for (char c : text.toCharArray()) {
             int idx = alphabet.indexOf(c);
             cipher.append(idx != -1 ? key.charAt(idx) : c);
         }
         System.out.println("Ciphertext: " + cipher);
+
+        StringBuilder plain = new StringBuilder();
+        for (char c : cipher.toString().toCharArray()) {
+            int idx = key.indexOf(c);
+            plain.append(idx != -1 ? alphabet.charAt(idx) : c);
+        }
+        System.out.println("Decrypted text: " + plain);
+
         sc.close();
     }
 }
